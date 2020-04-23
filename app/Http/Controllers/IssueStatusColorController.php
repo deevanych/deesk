4ed5c858c59a2c3fe17c3b3c8aafc9e79b321acd
Mikhail Zakharov <cashalotkzn@gmail.com>
2020-04-23 12:44:29 +0300

    init

---
+++ app/Http/Controllers/IssueStatusColorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\IssueStatusColor;
+use Illuminate\Http\Request;
+
+class IssueStatusColorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\IssueStatusColor  $issueStatusColor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(IssueStatusColor $issueStatusColor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\IssueStatusColor  $issueStatusColor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(IssueStatusColor $issueStatusColor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\IssueStatusColor  $issueStatusColor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, IssueStatusColor $issueStatusColor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\IssueStatusColor  $issueStatusColor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(IssueStatusColor $issueStatusColor)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/IssueStatusIconController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\IssueStatusIcon;
+use Illuminate\Http\Request;
+
+class IssueStatusIconController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\IssueStatusIcon  $issueStatusIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(IssueStatusIcon $issueStatusIcon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\IssueStatusIcon  $issueStatusIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(IssueStatusIcon $issueStatusIcon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\IssueStatusIcon  $issueStatusIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, IssueStatusIcon $issueStatusIcon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\IssueStatusIcon  $issueStatusIcon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(IssueStatusIcon $issueStatusIcon)
+    {
+        //
+    }
+}
