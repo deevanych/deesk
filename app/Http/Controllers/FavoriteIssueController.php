1240166be47d36ecf68c18b360bc136078659a74
/usr/bin/vim <support@beget.ru>
2020-05-01 13:58:24 +0300

    Добавлены миграция, модель, контроллер избранных заявок

---
+++ app/Http/Controllers/FavoriteIssueController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FavoriteIssue;
+use Illuminate\Http\Request;
+
+class FavoriteIssueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FavoriteIssue  $favoriteIssue
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FavoriteIssue $favoriteIssue)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FavoriteIssue  $favoriteIssue
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FavoriteIssue $favoriteIssue)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FavoriteIssue  $favoriteIssue
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FavoriteIssue $favoriteIssue)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FavoriteIssue  $favoriteIssue
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FavoriteIssue $favoriteIssue)
+    {
+        //
+    }
+}
